991432466a65f7a0816ae66372224fbdef076e6b
Nils Neumann <mail@der-zyklop.de>
2014-05-13 21:02:31 +0200

    added new site »watch«

---
+++ src/site/templates/watch.php
@@ -0,0 +1,34 @@
+<?php snippet('header') ?>
+
+  <main class="content" role="main">
+    <h1><?php echo html($page->title()) ?></h1>
+    <?php echo kirbytext($page->text()) ?>
+
+
+
+    <ul class="trip-stream">
+      <li>
+        Mannheim
+      </li>
+      <li>
+        Grüner Volvo / Fahrer: Weiblich
+      </li>
+      <li>
+        Darmstadt
+      </li>
+      <li>
+        Blauer Opel / Fahrer: Männlich ca. 40
+      </li>
+      <li>
+        Grüner Audi / Fahrer: Weiblich ca. 25
+      </li>
+      <li>
+        Gießen
+      </li>
+    </ul>
+
+
+
+  </main>
+
+<?php snippet('footer') ?>
