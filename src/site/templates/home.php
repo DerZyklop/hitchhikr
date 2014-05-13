0bc6f44fb979f8aff664d27e3a4778c495d315cd
Nils Neumann <mail@der-zyklop.de>
2014-05-13 15:10:24 +0200

    prepared some content

--- src/site/templates/home.php
+++ src/site/templates/home.php
@@ -3,11 +3,6 @@
   <main role="main">
     <h1><?php echo html($page->title()) ?></h1>
     <?php echo kirbytext($page->text()) ?>
-
-    <section>
-      <?php snippet('projects') ?>
-    </section>
-
   </main>
 
 <?php snippet('footer') ?>
