0bba63345b507e9fc538079e8e0201f3fd12beef
DerZyklop <mail@der-zyklop.de>
2014-05-22 22:19:22 +0200

    fixed #23

--- wireframe/src/site/snippets/trip-stream.php
+++ wireframe/src/site/snippets/trip-stream.php
@@ -3,10 +3,9 @@
   <ul>
     <li ng-class="{'unknown':$last}" ng-repeat="log in logs">
       {{log.time}} |
-      <span ng-if="log.location">{{log.location}}</span>
-      <span ng-if="log.color"> in a {{log.color}}-colored car</span>
-      <!-- TODO: this does't work. dont know why -->
-      <span ng-if="log.gender"> with a {{log.gender}}</span>
+      <span ng-if="log.location.length">{{log.location}}</span>
+      <span ng-if="log.color.length"> in a {{log.color}}-colored car</span>
+      <span ng-if="log.gender.length"> with a {{log.gender}}</span>
     </li>
     <li>
       {{trip.target}}
